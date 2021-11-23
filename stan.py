66020f69d55e9733520c09f3c92a3e5880813fe6
Kageshima <kageshimasu@docomo.ne.j>
2021-11-23 20:57:41 +0900

    add code by reference

--- stan.py
+++ stan.py
@@ -1,68 +1,81 @@
 import pystan
 import pandas as pd
+import numpy as np 
 
 
 def main():
     df = pd.read_csv('./data/extracted_data.csv')
-    
-    # 天気を0,1,2に変換
-    weather = df['weather'].tolist()
-    for i, w in enumerate(weather):
-        if w == 'sunny':
-            weather[i] = 0
-        elif w == 'cloudy':
-            weather[i] = 1
-        else:
-            weather[i] = 2
+
+    weather_mapping = {'sunny': 0, 'cloudy': 1, 'rainy': 2}
+    df['weather'] = df['weather'].map(weather_mapping)
+
+    target = list(df['target'] + 1)
+    df = df.drop(['target'], axis=1)
+    ones = np.ones(len(df.values))
+    df['temperature'] = df['temperature'] / 40
+    df['air_pressure'] = df['air_pressure'] / 1020
+    df['humidity'] = df['humidity'] / 100
+    df['bedtime'] = df['bedtime'] / 30
+    df['sleep_latency'] = df['sleep_latency'] / 5
+    df['sleep_duration'] = df['sleep_duration'] / 10
+    X = np.concatenate((ones.reshape(len(ones), 1), df.values),axis=1)
 
     data = {
         'N': len(df),
+        'D': X.shape[1],
         'K': 4,  # Kはカテゴリ数(今回は0,1,2,3の4つ)
-        'Y': list(df['target'] + 1),
-        'is_workday': list(df['is_working']),
-        'weather': weather,
-        'temperature': list(df['temperature']),
-        'air_pressure': list(df['air_pressure']),
-        'humidity': list(df['humidity']),
-        'study_in_morning': list(df['study_in_morning']),
-        'go_out': list(df['go_out']),
-        'bedtime': list(df['bedtime']),
-        'sleep_latency': list(df['sleep_latency']),
-        'sleep_duration': list(df['sleep_duration'])
+        'Y': target,
+        'X': X
     }
+
+    # df = pd.read_csv('./data/data2.csv')
+    # target = list(df['Y'])   
+    # df = df.drop(['Y'], axis=1)
+    # df['Age'] = df['Age'] / 100
+    # df['Income'] = df['Income'] / 1000
+    # ones = np.ones(len(df.values))
+    # X = np.concatenate((ones.reshape(len(ones), 1), df.values),axis=1)
+    # print(X)
+
+    # data = {
+    #     'N': len(df),
+    #     'D': X.shape[1],
+    #     'K': 6,
+    #     'Y': target,
+    #     'X': X
+    # }
 
     code = """
         data {
             int N;
+            int D;
             int K;
+            matrix[N,D] X;
             int<lower=1, upper=K> Y[N];
-            int<lower=0, upper=1> is_workday[N];
-            int<lower=0, upper=2> weather[N];
-            real temperature[N];
-            real air_pressure[N];
-            real humidity[N];
-            int<lower=0, upper=1> study_in_morning[N];
-            int<lower=0, upper=1> go_out[N];
-            real bedtime[N];
-            real sleep_latency[N];
-            real sleep_duration[N];
+        }
+
+        transformed data {
+            vector[D] Zeros;
+            Zeros = rep_vector(0,D);
         }
 
         parameters {
-            matrix[K, 11] b;
+            matrix[D,K-1] b_raw;
         }
 
         transformed parameters {
-            simplex[K] theta[N];
-            for (n in 1:N)
-                theta[n] = softmax(b[,1] + b[,2]*is_workday[n] + b[,3]*weather[n] + b[,4]*temperature[n] + b[,5]*air_pressure[n] + b[,6]*humidity[n] + b[,7]*study_in_morning[n] + b[,8]*go_out[n] + b[,9]*bedtime[n] + b[,10]*sleep_latency[n] + b[,11]*sleep_duration[n]);
+            matrix[D,K] b;
+            matrix[N,K] mu;
+            b = append_col(Zeros, b_raw);
+            mu = X*b;
         }
 
         model {
             for (n in 1:N)
-                Y[n] ~ categorical(theta[n]);
+                Y[n] ~ categorical(softmax(mu[n,]'));
         }
     """
+
     model = pystan.StanModel(model_code=code)
     fit = model.sampling(data=data, iter=1000, chains=3)
     la = fit.extract(permuted=True)
